f5c3a7fc576d694cbd8c757edf06f42b29384b72
NicoCercio12 <cercio1998@gmail.com>
2026-03-23 15:19:27 -0300

    Commit correcto

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -7,7 +7,15 @@
         public string Descripcion { get; set; }
         public DateTime FechaCreacion { get; set; }
 
+        public Tarea(int Id, string Titulo, string Descripcion, DateTime FechaCreacion) {
 
+                this.Id = Id;
+                this.Titulo = Titulo;
+                this.Descripcion = Descripcion;
+                this.FechaCreacion = FechaCreacion;
+
+
+        }
 
     }
 }
--- Services/TareaService.cs
+++ Services/TareaService.cs
@@ -1,16 +1,62 @@
 ﻿using ListaDeTareas.Data;
+using ListaDeTareas.Models;
 
 namespace ListaDeTareas.Services
 {
-    public class TareaServices
+    public class TareaService
     {
-        
-        public TareaServices() { }
+
+        private readonly AppDbContext _context;
+
+        public TareaService(AppDbContext context)
+        {
+            _context = context;
+        }
+
 
         public bool AgregarTarea(string Titulo, string Descripcion, DateTime FechaCreacion)
         {
+            List<Tarea> tareas = new List<Tarea>();
 
+            int i = 0;
+            bool existe = false;
+
+            while (i < tareas.Count && !existe)
+            {
+                Tarea t = tareas[i];
+
+                if (string.Equals(t.Titulo, Titulo, StringComparison.OrdinalIgnoreCase) && string.Equals(t.Descripcion, Descripcion, StringComparison.OrdinalIgnoreCase))
+                {
+                    existe = true;
+                }
+
+                i++;
+            }
+
+            if (existe)
+            {
+                throw new Exception("La tarea ya existe");
+            }
+
+            _context.Tareas.Add(new Tarea(0, Titulo, Descripcion, FechaCreacion));
+            _context.SaveChanges();
+
+            return true;
         }
+
+        public List<Tarea> ListarTareas()
+        {
+            List<Tarea> tareas = new List<Tarea>();
+
+            foreach (Tarea t in _context.Tareas)
+            {
+                tareas.Add(t);
+            }
+
+            return tareas;
+        }
+
+
 
     }
 }
